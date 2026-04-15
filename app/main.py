387fdd88f49c78412ffc48eabc106af80d7f6b9c
Miremma <veramiremmafoh@etudiant.ief2i.fr>
2026-04-15 17:27:39 +0200

    fix cors

--- app/main.py
+++ app/main.py
@@ -29,6 +29,14 @@ app = FastAPI(
     description="API pour la plateforme de stages et alternances Stagify",
     lifespan=lifespan
 )
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://stagify-front-production.up.railway.app"],  # ton front
+    allow_credentials=True,
+    allow_origins=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
 
 
 # ════════════════════════════════════════
